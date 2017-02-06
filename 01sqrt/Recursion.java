a92fe8c759ab41746740a604b6602e9eb1f52bf7
Stanley Lin <slin55055@gmail.com>
2017-02-06 12:32:42 -0500

    started hw01

---
+++ 01sqrt/Recursion.java
@@ -0,0 +1,19 @@
+public class Recursion {
+	public static String name() {
+		return "Lin,Stanley";
+	}
+	
+	private static double approximate(	double n,
+						double guess) {
+		if (doubEq(n, guess*guess)) {
+		}
+	}
+	
+	public static double sqrt(double n) {
+		return approximate(n, 1);
+	} 
+	
+	public static void main(String args[]) {
+		
+	}
+}
