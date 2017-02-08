070397955672bbae5c75de0ff71534f1803560a9
Stanley Lin <slin55055@gmail.com>
2017-02-08 12:33:29 -0500

    Started nqueens

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,28 @@
+public class QueenBoard {
+	private int[][] board;
+	private int solutionCount;
+	
+	public QueenBoard(int size) {
+		board = new int[size][size];
+		solutionCount = -1;
+	}
+	
+	public boolean solve() {
+	}
+	
+	public boolean countSolutions() {
+	}
+	
+	public String getCount() {
+	}
+	
+	public String toString() {
+		for (int[] r : board) {
+			for (int elem : r)
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+}
