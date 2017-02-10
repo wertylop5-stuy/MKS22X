b23cc3805411a1b9a7da782c85997a417c40619b
Stanley Lin <slin55055@gmail.com>
2017-02-09 19:37:19 -0500

    QueenBoard finds one solution

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,28 +1,147 @@
 public class QueenBoard {
 	private int[][] board;
-	private int solutionCount;
+	private int mSolutions;
 	
 	public QueenBoard(int size) {
 		board = new int[size][size];
-		solutionCount = -1;
+		mSolutions = -1;
+		//board[0][0] = 1;
 	}
 	
-	public boolean solve() {
-	}
-	
-	public boolean countSolutions() {
-	}
-	
-	public String getCount() {
-	}
-	
-	public String toString() {
-		for (int[] r : board) {
-			for (int elem : r)
+	//number is the amount to add to each cell
+	/* Will affect every row, col, and diag, but will leave inputted space
+	alone */
+	private void modify(int row, int col, int number) {
+		//check the columns in row
+		for (int x = 0; x < board.length; x++) {
+			if ( (x != col && board[row][x] == -1) ||
+				(x != row && board[x][col] == -1) ) {
+				System.out.println(this);
+				throw new IllegalStateException("y dis neg");
+			}
+			
+			//modify the columns in row
+			if (board[row][x] != -1) {
+				board[row][x] += number;
+			}
+			
+			//modify the rows in col
+			if (board[x][col] != -1) {
+				board[x][col] += number;
+			}
+		}
+		
+		int tRow, tCol;
+		//negative diagonal
+		//could make this better by adding 7 to a counter variable
+		tRow = row+1;
+		tCol = col+1;
+		while(tRow < board.length && tCol < board.length) {
+			board[tRow++][tCol++] += number; 
+		}
+		tRow = row-1;
+		tCol = col-1;
+		while(tRow > -1 && tCol > -1) {
+			board[tRow--][tCol--] += number;
+		}
+		
+		//positive diagonal
+		tRow = row-1;
+		tCol = col+1;
+		while (tRow > -1 && tCol < board.length) {
+			board[tRow--][tCol++] += number;
+		}
+		tRow = row+1;
+		tCol = col-1;
+		while (tRow < board.length && tCol > -1) {
+			board[tRow++][tCol--] += number;
 		}
 	}
 	
+	private boolean addQueen(int row, int col) {
+		if (board[row][col] != 0) {
+			return false;
+		}
+		else {
+			modify(row, col, 1);
+			board[row][col] = -1;
+			return true;
+		}
+	}
+	
+	private void remQueen(int row, int col) {
+		if (board[row][col] != -1) return;
+		modify(row, col, -1);
+		board[row][col] = 0;
+	}
+	
+	private boolean recursiveSolve(int row, int numQueens) {
+		if (row == board.length) return numQueens == board.length;
+		else {
+			for(int elem = 0; elem < board.length; elem++) {
+				if (addQueen(row, elem)) {
+					if (recursiveSolve(row+1, numQueens+1)) return true;
+					remQueen(row, elem);
+				}
+			}
+			return false;
+		}
+	}
+	
+	public boolean solve() {
+		return recursiveSolve(0, 0);
+	}
+	
+	public boolean countSolutions() {
+		return true;
+	}
+	
+	public int getCount() {
+		return mSolutions;
+	}
+	
+	public String toString() {
+		String res = "";
+		for (int[] r : board) {
+			for (int elem : r) {
+				if (elem == -1) res = res + "Q ";
+				else res = res + elem + " ";
+			}
+			res += "\n";
+		}
+		return res;
+	}
+	
 	public static void main(String args[]) {
+		QueenBoard q = new QueenBoard(8);
+		//System.out.println(q);
+		/*q.addQueen(0, 0);
+		System.out.println(q);
 		
+		System.out.println("same");
+		q.addQueen(0, 0);
+		System.out.println(q);
+		
+		System.out.println("same");
+		q.addQueen(0, 1);
+		System.out.println(q);
+		
+		System.out.println("modif");
+		q.addQueen(2, 1);
+		System.out.println(q);
+		
+		System.out.println("modif");
+		q.remQueen(0, 0);
+		System.out.println(q);
+		
+		System.out.println("same");
+		q.remQueen(0, 0);
+		System.out.println(q);
+		
+		System.out.println("modif");
+		q.remQueen(2, 1);
+		System.out.println(q);*/
+		q.solve();
+		System.out.println(q);
 	}
 }
