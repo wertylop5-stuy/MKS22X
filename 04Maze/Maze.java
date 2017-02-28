3b6ef18194ffe71dc2c1fca6677dd7f1108a73c5
Stanley Lin <slin55055@gmail.com>
2017-02-28 12:37:04 -0500

    Almost done with maze constructor

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Maze {
     private char[][] maze;
     private boolean animate;
+	private int mCols = -1;
 
     /*
       Constructor loads a maze text file, and sets animate to false by default.
@@ -16,15 +17,37 @@ public class Maze {
     */
 
     public Maze(String filename){
-	Scanner s;
+	Scanner input = null;
 	try {
-		s = new Scanner(new FileInputStream(filename));
+		input = new Scanner(new FileInputStream(filename));
 	}
 	catch (FileNotFoundException e) {
 		System.err.println("file " + filename + " does not exist");
 		System.exit(1);
 	}
+	
+	String s;
+	List<String> temp = new ArrayList<>();
+	while(input.hasNext()) {
+		s = input.nextLine();
+		//System.out.println(s);
+		mCols = (mCols == -1) ? s.length() : mCols;
+		temp.add(s);
+	}
+	
+	maze = new char[temp.size()][mCols];
+	
+	//Now move from temp to maze array
+	for (int x = 0; x < temp.size(); x++) {
+		maze[x] = temp.get(0).toCharArray();
+	}
+	System.out.println(maze[0][0]);
     }
+
+	private void wait(int millis) {
+		try { Thread.sleep(millis); }
+		catch (InterruptedException e){}
+	}
 
     public void setAnimate(boolean b) { animate = b; }
 
@@ -37,7 +60,7 @@ public class Maze {
         All visited spots that were not part of the solution are changed to '.'
         All visited spots that are part of the solution are changed to '@'
     */
-    private boolean solve(int x, int y) throws InterruptedException {
+    private boolean solve(int x, int y) {
         if(animate){
             System.out.println(this);
             wait(20);
@@ -51,7 +74,7 @@ public class Maze {
     /*Wrapper Solve Function
       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
     */
-    public boolean solve() throws InterruptedException {
+    public boolean solve() {
             int startx=0,starty=0;
 
             //Initialize startx and starty with the location of the S. 
@@ -59,7 +82,7 @@ public class Maze {
             return solve(startx,starty);
     }
 	
-	public static void main(String args[]) throws InterruptedException {
+	public static void main(String args[]) {
 		Maze m = new Maze("data1.dat");
 		m.solve();
 	}
