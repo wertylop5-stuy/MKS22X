adc30a5da0f5a54d9b33101ad40b3c6f65b4b11d
Stanley Lin <slin55055@gmail.com>
2017-03-06 12:32:47 -0500

    work on USACO

--- USACO/mar08bronze.java
+++ USACO/mar08bronze.java
@@ -2,7 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class mar08bronze {
-	public static void main() {
+	
+	public static void stomp(int rs, int cs, int ds, int[][] lake) {
+		int highX, highY;
+		int hi = -999;
+		for (int x = rs; x < rs+3; x++) {
+			for (int y = cs; y<cs+3; y++) {
+				if (lake[x][y] > hi) {
+					hi = lake[x][y];
+					highX = x;
+					highY = y;
+				}
+			}
+		}
+		System.out.println(hi);
+	}
+	
+	public static void main(String args[]) throws Exception{
 		BufferedReader in = null;
 		PrintWriter out = null;
 		try{
@@ -13,10 +29,38 @@ public class mar08bronze {
 		catch(Exception e){System.out.println("Exception");}
 
 		StringTokenizer st = null;
-		String row = null;
 		
 		int r, c, e, n;
-		st = new Stringtokenizer(in.readLine());
+		try{
+		st = new StringTokenizer(in.readLine());
+		}catch(Exception ex){}
+		
 		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		
+		int[][] lake = new int[r][c];
+		for(int row = 0; row < r; row++) {
+			try{
+			st = new StringTokenizer(in.readLine());}
+			catch (Exception ex){}
+			
+			for(int col = 0; col < c; col++){
+				lake[row][col] = Integer.parseInt(st.nextToken());
+//System.out.println(lake[row][col]);
+			}
+		}
+int rs, cs, ds;
+for(int x = 0; x<n; x++){
+st = new StringTokenizer(in.readLine());
+rs = Integer.parseInt(st.nextToken()) - 1;
+cs = Integer.parseInt(st.nextToken()) - 1;
+ds = Integer.parseInt(st.nextToken());
+	stomp(rs, cs, ds, lake);
+}
+
+
 	}
+
 }
