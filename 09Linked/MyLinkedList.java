26e76c1f8745fcb113aa6d05597df46a1d2a7c58
Stanley Lin <slin55055@gmail.com>
2017-03-28 12:31:48 -0400

    finished phase 1 methods

--- 09Linked/MyLinkedList.java
+++ 09Linked/MyLinkedList.java
@@ -12,6 +12,10 @@ public class MyLinkedList {
 			data = d;
 			next = n;
 		}
+		
+		public Node(int d) {
+			data = d;
+		}
 		/*
 		public void setData(int d) {data = d;}
 		public void setNode(Node n) {next = n;}
@@ -23,8 +27,8 @@ public class MyLinkedList {
 	
 	public MyLinkedList() {}
 	
-	public void addHead(int data) {
-		Node t = new Node(data, null);
+	public boolean addHead(int data) {
+		Node t = new Node(data);
 		if (head != null) {
 			t.next = head;
 		}
@@ -33,9 +37,23 @@ public class MyLinkedList {
 		head = t;
 		
 		size++;
+		return true;
+	}
+	
+	//adds to end of list
+	public boolean add(int val) {
+		Node t = new Node(val);
+		tail.next = t;
+		tail = t;
+		
+		size++;
+		return true;
 	}
 	
 	public void add(int pos, int data) {
+		if (pos < 0 || pos > data)
+			throw new IndexOutOfBoundsException(""+pos);
+		
 		if (pos == 0) {
 			addHead(data);
 			return;
@@ -57,6 +75,44 @@ public class MyLinkedList {
 		}
 		
 	}
+	
+	private Node getRef(int index) {
+		if (index < 0 || index > size)
+			throw new IndexOutOfBoundsException(""+index);
+		
+		Node ptr = head;
+		while (index > 0) {
+			ptr = ptr.next;
+			index--;
+		}
+		return ptr;
+	}
+	
+	public int get(int index) {
+		return getRef(index).data;
+	}
+	
+	public int set(int index, int newVal) {
+		Node t = getRef(index);
+		int old = t.data;
+		t.data = newVal;
+		return old;
+	}
+	
+	public int indexOf(int val) {
+		Node ptr = head;
+		int counter = 0;
+		
+		while(ptr != null || counter < size) {
+			if (ptr.data == val) return counter;
+			
+			ptr = ptr.next;
+			counter++;
+		}
+		return -1;
+	}
+	
+	public int size() { return size; }
 	
 	@Override
 	public String toString() {
@@ -81,5 +137,14 @@ public class MyLinkedList {
 		l.addHead(7);
 		l.addHead(11);
 		System.out.println(l);
+		l.add(6);
+		System.out.println(l);
+		l.add(6);
+		System.out.println(l);
+		
+		/*
+		System.out.println(l.set(4, 99));
+		System.out.println(l);
+		*/
 	}
 }
