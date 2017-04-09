c89a565816db537af9136bddde631c1976808cc6
Stanley Lin <slin55055@gmail.com>
2017-04-09 15:59:17 -0400

    add methods work

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -4,12 +4,25 @@ public class MyDeque {
 	private int back;
 	
 	public MyDeque() {
-		dq = new String[8];
+		dq = new String[4];
+		back = 2;
+		front = 3;
 	}
 	
 	private void resize() {
 		String temp[] = new String[dq.length * 2];
-		int counter;
+		int counter = 0;
+		for (int x = back+1; x != front; x=(x+1)%dq.length, counter++) {
+			temp[counter] = dq[x];
+		}
+		System.out.println("resized");
+		for (String s : temp) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+		dq = temp;
+		back = temp.length-1;
+		front = counter;
 	}
 	
 	public void addFirst(String s) {
@@ -17,9 +30,24 @@ public class MyDeque {
 		if (front == back) resize();
 		dq[front++] = s;
 		front %= dq.length;
+		
+		for (String e : dq) {
+			System.out.print(e + " ");
+		}
+		System.out.println();
 	}
 	
-	public void addLast(String s) {}
+	public void addLast(String s) {
+		if (s == null) throw new NullPointerException();
+		if (front == back) resize();
+		dq[back--] = s;
+		if (back < 0) back = back+dq.length;
+		
+		for (String e : dq) {
+			System.out.print(e + " ");
+		}
+		System.out.println();
+	}
 	
 	public String removeFirst() {
 		return null;
@@ -41,13 +69,33 @@ public class MyDeque {
 	@Override
 	public String toString() {
 		String res = "[";
-		for (String s : dq) {
-			if (s == null) res+="_, "
+		for (int x = (back+1)%dq.length; x != front; x = (x+1)%dq.length) {
+			System.out.println(x+": "+dq[x]);
+			if (dq[x] == null) res += "_, ";
+			else res += dq[x] + ", ";
 		}
-		if (res.length > 1) return res.substring(res-
+		//System.out.println(res+res.length());
+		if (res.length() > 1) return res.substring(0, res.length()-2)+"]";
+		return res+"]";
 	}
 	
 	public static void main(String args[]) {
 		MyDeque d = new MyDeque();
+		System.out.println(d);
+		d.addFirst("a");
+		System.out.println(d);
+		d.addFirst("b");
+		d.addFirst("e");
+		System.out.println(d);
+		d.addFirst("g");
+		d.addFirst("u");
+		System.out.println(d);
+		d.addLast("y");
+		System.out.println(d);
+		d.addLast("x");
+		System.out.println(d);
+		d.addLast("q");
+		System.out.println(d);
+		System.out.println("first: "+d.getFirst()+" last: "+d.getLast());
 	}
 }
