b13d212df85ab1a5268e36ba64dcd9658932f153
Stanley Lin <slin55055@gmail.com>
2017-05-04 12:18:37 -0400

    resizing works

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,29 +1,75 @@
 public class MyHeap {
 	private String[] heap;
+	private int size;
+	private int pos;
+	private boolean isMax;
 	
 	public MyHeap() {
+		this(true);
+	}
+	
+	public MyHeap(boolean b) {
 		heap = new String[2];
+		isMax = b;
+		pos = 1;
+		size = 0;
 	}
 	
 	private void resize() {
-		String[] res = new String[heap.length];
+		//System.out.println("resize");
+		String[] res = new String[heap.length*2];
 		System.arraycopy(heap, 0, res, 0, heap.length);
 		heap = res;
 	}
 	
-	public void add() {
-		
+	//bumps up leaf elem if greater than parent
+	private void pushUp(int pos) {
+		while (pos > 1 && heap[pos] >= heap[pos/2]) {
+			
+		}
 	}
 	
-	public void remove() {
+	public void add(String s) {
+		//System.out.println(size + " " + pos + " " + heap.length);
+		if (size >= heap.length - 1) {
+			resize();
+		}
+		heap[pos++] = s;
+		size++;
 		
+		pushUp(pos-1);
 	}
 	
-	public void peek() {
-		
+	public String remove() {
+		return null;
+	}
+	
+	public String peek() {
+		return null;
+	}
+	
+	public String toString() {
+		String res = "[";
+		for (String s : heap) {
+			if (s == null) res += "_ ";
+			else res += s + " ";
+		}
+		if (res.length() > 1)
+			return res.substring(0, res.length()-1) + "]";
+		return res + "]";
 	}
 	
 	public static void main(String[] args) {
+		MyHeap m = new MyHeap();
+		System.out.println(m);
+		m.add("t");
+		System.out.println(m);
 		
+		m.add("r");
+		System.out.println(m);
+		m.add("s");
+		System.out.println(m);
+		m.add("t");
+		System.out.println(m);
 	}
 }
