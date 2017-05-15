9c75a394d9f47996b9edefa93c4b33bf68046fc5
Stanley Lin <slin55055@gmail.com>
2017-05-15 12:37:11 -0400

    Start 15

---
+++ 15Maze2/Location.java
@@ -0,0 +1,39 @@
+public class Location implements Comparable<Location> {
+	private int row;
+	private int col;
+	private Location previous;
+	private int distToStart;
+	private int distToGoal;
+	private boolean aStar;
+	
+	public Location(int r, int c, Location prev,
+		int dts, int dtg) {
+		this(r, c, prev, dts, dtg, false);
+	}
+	
+	public Location(int r, int c, Location prev,
+		int dToStart, int dToGoal, boolean as) {
+		row = r; col = c;
+		previous = prev;
+		distToStart = dToStart;
+		distToGoal = dToGoal;
+		aStar = as;
+	}
+	
+	//Greater means it is farther/worse
+	@Override
+	public int compareTo(Location other) {
+		int thisDist = distToGoal + 
+			(aStar ? previous.distToStart : 0);
+		
+		int otherDist = other.distToGoal + 
+			(aStar ? other.previous.distToStart : 0);
+		return thisDist - otherDist;
+	}
+	
+	public static void main(String[] args) {
+		Location a = new Location(0, 0, null, 0, 5);
+		Location b = new Location(0, 0, a, 0, 5);
+		Location c = new Location(0, 0, null, 0, 5);
+	}
+}
